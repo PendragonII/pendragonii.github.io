49710615fedce14f2ff5ecb374d901c954520640
PendragonII <>
2021-01-07 16:41:09 -0500

    JQuery assistance

--- vars-import.js
+++ vars-import.js
@@ -1,14 +1,20 @@
-function scriptCreate(scriptCreateName) {
-    var scriptCreateArg = document.createElement('script');
-    scriptCreateArg.src=scriptCreateName;
-    document.head.appendChild(scriptCreateArg);
-}
-function cssCreate(cssCreateName) {
-    var cssCreateArg = document.createElement('link');
-    cssCreateArg.rel="stylesheet";
-    cssCreateArg.type="text/css";
-    cssCreateArg.href=cssCreateName;
-    document.head.appendChild(cssCreateArg);
-}
+$(function(){
+    // Insert MainMenu and Footer
+    $("#imports").load("vars.html");
+});
 
-scriptCreate("vars.js");
+
+// function scriptCreate(scriptCreateName) {
+//     var scriptCreateArg = document.createElement('script');
+//     scriptCreateArg.src=scriptCreateName;
+//     document.head.appendChild(scriptCreateArg);
+// }
+// function cssCreate(cssCreateName) {
+//     var cssCreateArg = document.createElement('link');
+//     cssCreateArg.rel="stylesheet";
+//     cssCreateArg.type="text/css";
+//     cssCreateArg.href=cssCreateName;
+//     document.head.appendChild(cssCreateArg);
+// }
+
+// scriptCreate("vars.js");
